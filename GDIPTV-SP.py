f770870108253f96eff94737095988aa147d34d8
ngdikman <diwen.0906@163.com>
2024-11-14 10:34:50 +0800

    Update GDIPTV-SP.py

--- GDIPTV-SP.py
+++ GDIPTV-SP.py
@@ -4,11 +4,36 @@ import random
 import base64
 import os
 
+user_agent = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3100.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+        'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+        'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0',
+        'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)',
+        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
+        'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+        'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
+        'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
+        'Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)',
+        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)',
+        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; The World)',
+        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
+        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)',
+        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Avant Browser)',
+        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+    ]
+
 cookie = os.getenv("FOFA_COOKIE")
+User_Agent = user_agent[random.randint(0, len(user_agent) - 1)]
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+    'User-Agent': User_Agent,
     'Accept': 'application/json, text/plain, */*',
-    'Authorization':cookie
+    'Authorization':FOFA_COOKIE
 }
 
 # 定义需要爬取的城市
@@ -18,7 +43,7 @@ urls_all = []
 # 通过 Fofa 获取 IP 地址
 for url in urls:
     url_0 = str(base64.b64encode((f'"Server: udpxy" && city="{url}" && org="Chinanet"').encode("utf-8")), "utf-8")
-    url_64 = f'https://fofa.info/result?qbase64={url_0}'
+    url_64 = f'https://fofa.so/result?&qbase64={url_0}'
 
     try:
         response = requests.get(url_64, headers=headers, timeout=15)
@@ -120,7 +145,7 @@ def main():
     if valid_addresses:
         selected_address = random.choice(valid_addresses)
         replace_m3u_file('GDIPTV.m3u', selected_address)
-        replace_m3u_file('GDIPTV-SP.m3u', selected_address)
+        # replace_m3u_file('GDIPTV-SP.m3u', selected_address)
     else:
         print("没有可用的频道地址")
 
