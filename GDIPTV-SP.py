d3ee2c7a1dc6e72bc90c3844f4e92a2c74c1a653
ngdikman <diwen.0906@163.com>
2024-11-28 17:03:53 +0800

    Update GDIPTV-SP.py

--- GDIPTV-SP.py
+++ GDIPTV-SP.py
@@ -2,6 +2,16 @@ import requests
 import re
 import random
 
+def get_public_ip():
+    try:
+        response = requests.get("https://httpbin.org/ip")
+        response.raise_for_status()
+        ip = response.json().get("origin")
+        return ip
+    except requests.RequestException as e:
+        print(f"Error fetching IP: {e}")
+        return None
+
 user_agent = [
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3100.0 Safari/537.36',
@@ -40,23 +50,24 @@ urls = {"guangzhou":"https://fofa.info/result?qbase64=IlNlcnZlcjogdWRweHkiICYmIG
         "qingyuan":"https://fofa.info/result?qbase64=IlNlcnZlcjogdWRweHkiICYmIGNpdHk9InFpbmd5dWFuIiAmJiBvcmc9IkNoaW5hbmV0Ig%3D%3D"
         }
 urls_all = []
+print("当前IP为:", get_public_ip())
 
 # 通过 Fofa 获取 IP 地址
 for city, url in urls.items():
     try:
-        response = requests.get(url, headers=headers, timeout=10)
+        response = requests.get(url, headers=headers, timeout=15)
         page_content = response.text
         print(f" {city} 访问成功")
 
         # 使用正则提取 IP 地址
-        pattern = r'href="(http:\/\/\d+\.\d+\.\d+\.\d+:\d+)"'
+        pattern = r'href="http:\/\/(\d+\.\d+\.\d+\.\d+:\d+)"'
         page_urls = re.findall(pattern, page_content)
 
         for urlx in page_urls:
             try:
                 # 检测链接是否可用
-                response = requests.get(url=f'{urlx}/status', timeout=10)
-                response.raise_for_status() 
+                response = requests.get(url=f'http://{urlx}/status', timeout=1)
+                response.raise_for_status()  # 如果状态码不是 200 会引发异常
                 page_content = response.text
 
                 # 判断页面是否包含有效的内容
@@ -128,7 +139,7 @@ def save_addresses_to_file(channel_addresses, filename='ip-port.txt'):
 def main():
     # 合并新的和已有的频道地址并去重
     existing_addresses = read_addresses_from_file('ip-port.txt')
-    all_addresses = channel_addresses + existing_addresses
+    all_addresses = list(set(channel_addresses + existing_addresses))
 
     # 检测合并后的频道地址可用性
     valid_addresses = [addr for addr in all_addresses if test_m3u(addr)]
