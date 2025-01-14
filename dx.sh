f93703283ebf0580f24c224158693ac57e7006f6
pp <>
2025-01-14 11:30:16 +0800

    Signed-off-by: pp <>

--- dx.sh
+++ dx.sh
@@ -52,10 +52,19 @@ rm -f "speedtest_${city}_$time.log"
 
 # 用 3 个最快 ip 生成对应城市的 txt 文件
 program="template/template_dx.m3u"
+txtprogram="template/template_dx.txt"
 
 sed "s/ipipip/$ip1/g" "$program" > tmp1.txt
 sed "s/ipipip/$ip2/g" "$program" > tmp2.txt
-#sed "s/ipipip/$ip3/g" "$program" > tmp3.txt
 cat tmp1.txt > "GDIPTV.m3u"
 cat tmp2.txt > "GDIPTV-SP.m3u"
-rm -rf tmp1.txt tmp2.txt tmp3.txt
+
+sed "s/ipipip/$ip1/g" "$txtprogram" > tmp3.txt
+sed "s/ipipip/$ip2/g" "$txtprogram" > tmp4.txt
+cat tmp3.txt tmp4.txt > "txt/fofa_${city}.txt"
+
+rm -rf tmp1.txt tmp2.txt tmp3.txt tmp4.txt
+rm -rf dianxin.txt
+
+curl -s https://aktv.top/live.txt >>dianxin.txt
+cat txt/fofa_dianxin.txt >>dianxin.txt
